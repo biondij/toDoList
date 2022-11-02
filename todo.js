f80ae56dc71cb4c80055f6838b3191b29d4324f9
Julia Biondi <100867312+biondij@users.noreply.github.com>
2022-11-02 11:33:58 -0300

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,24 @@
+let input = prompt("O que você gostaria de fazer?");
+const todos = ["Passar aspirador no quarto", "Preparar o almoço"];
+while (input !== "quit" && input !== "q") {
+    if (input === "list") {
+        console.log("*******************");
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`) // possível fazer com for each
+        }
+        console.log("*******************");
+    } else if (input === "new") {
+        const newTodo = prompt("Qual afazer você deseja adicionar?");
+        todos.push(newTodo);
+        console.log(`${newTodo} adicionado aos afazeres`);
+    } else if (input === "delete") {
+        const index = parseInt(prompt("Digite o index do afazer que você deseja deletar"));
+        if (!Number.isNaN) {
+            const deleted = todos.splice(index, 1);
+            console.log(`${deleted[0]} deletado dos afazeres`);
+        } else {
+            console.log("Index inválido");
+        }
+    }
+    input = prompt("O que você gostaria de fazer?");
+} console.log("Até logo!")
